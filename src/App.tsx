6b22f95aade95b0adde49ca608b7db8862688336
PhilipeF <philipesferreiraa@gmail.com>
2024-07-26 10:52:35 -0300

    Feat: Enviando resolução do exercício

--- src/App.tsx
+++ src/App.tsx
@@ -2,16 +2,18 @@ import Header from './components/Cabecalho'
 import Hero from './components/Hero'
 import ListaVagas from './containers/ListaVagas'
 
-import './global.css'
+// import './global.css'
+import GlobalStyle, { Container } from './styles'
 
 function App() {
   return (
     <>
+      <GlobalStyle />
       <Header />
       <Hero />
-      <div className="container">
+      <Container>
         <ListaVagas />
-      </div>
+      </Container>
     </>
   )
 }
